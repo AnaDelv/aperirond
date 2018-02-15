6f8bc5929a7f105d43c7269a3d255d007ea63da1
unknown <anaelle.delavau@imie.fr>
2018-02-15 09:22:50 +0100

    Gestion formulaire

--- inscription.php
+++ inscription.php
@@ -16,7 +16,19 @@
 </header>
 <section>
     <div class="container shadow corps">
-        Inscription
+        <div class="row">
+            <div class="col-12 center">
+                <form method="POST" action="register.php">
+                    <p>Votre pseudo:</p>
+                    <input type="text" name="username">
+                    <p>Votre password</p>
+                    <input type="password" name="password">
+                    <p>Répétez votre password</p>
+                    <input type="password" name="repeatpassword">
+                    <input type="submit" value="S'inscrire" name="submit">
+                </form>
+            </div>
+        </div>
     </div>
 </section>
 </body>
